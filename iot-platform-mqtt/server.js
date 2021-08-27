8d8522910fb008f3f7a411c6a1bb14590a539caf
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-26 19:58:03 -0500

    feat(iot-platform-mqtt): integración del servicio iot-platform-db

--- iot-platform-mqtt/server.js
+++ iot-platform-mqtt/server.js
@@ -3,6 +3,7 @@ const debug = require('debug')('iot-platform:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('iot-platform-db')
 
 const backend = {
   type: 'redis',
@@ -15,7 +16,18 @@ const settings = {
   backend
 }
 
+const config = {
+  database: process.env.DB_NAME || 'iot_platform',
+  username: process.env.DB_USER || 'iot_platform',
+  password: process.env.DB_PASSWORD || 'iot_platform',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
+}
+
 const server = new mosca.Server(settings)
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
@@ -30,13 +42,16 @@ server.on('published', (packet, client) => {
   debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
   console.log(`${chalk.green('[iot-platform-mqtt]')} server is running`)
 })
 
 server.on('error', handleFatalError)
 
-function handleFatalError(err){
+function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
