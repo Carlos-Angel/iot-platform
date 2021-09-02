f3b291c3ba185ff17051efc1ca60c7a3ab555c68
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-02 09:47:34 -0500

    feat(iot-platform-agent): implementando un custom EventEmitter

---
+++ iot-platform-agent/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const EventEmitter = require('events')
+
+class IotPlatformAgent extends EventEmitter {
+  constructor () {
+    super()
+  }
+}
+
+module.exports = IotPlatformAgent
