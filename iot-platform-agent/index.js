85f64e8ec5a9c8f7b9f0900dc1c9caffd5d2c20b
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 00:03:41 -0500

    feat(iot-platform-agent): implementación de metricas personalizadas

--- iot-platform-agent/index.js
+++ iot-platform-agent/index.js
@@ -1,5 +1,7 @@
 'use strict'
 const debug = require('debug')('iot-platform:agent')
+const os = require('os')
+const util = require('util')
 const mqtt = require('mqtt')
 const defaults = require('defaults')
 const uuid = require('uuid')
@@ -12,18 +14,28 @@ const options = {
   username: 'iot-platform',
   interval: 5000,
   mqtt: {
-    host: 'mqtt:://localhost'
+    host: 'mqtt://localhost'
   }
 }
 
 class IotPlatformAgent extends EventEmitter {
   constructor (opts) {
     super()
+
     this._options = defaults(opts, options)
     this._started = false
     this._timer = null
     this._client = null
     this._agentId = null
+    this._metrics = new Map()
+  }
+
+  addMetric (type, fn) {
+    this._metrics.set(type, fn)
+  }
+
+  removeMetric (type) {
+    this._metrics.delete(type)
   }
 
   connect () {
@@ -41,8 +53,36 @@ class IotPlatformAgent extends EventEmitter {
 
         this.emit('connected', this._agentId)
 
-        this._timer = setInterval(() => {
-          this.emit('agent/message', 'this is a message')
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) {
+            let message = {
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
+
+            for (let [ metric, fn ] of this._metrics) {
+              if (fn.length === 1) {
+                fn = util.promisify(fn)
+              }
+
+              message.metrics.push({
+                type: metric,
+                value: await Promise.resolve(fn())
+              })
+            }
+
+            debug('Sending', message)
+
+            this._client.publish('agent/message', JSON.stringify(message))
+            this.emit('message', message)
+          }
         }, opts.interval)
       })
 
@@ -50,7 +90,6 @@ class IotPlatformAgent extends EventEmitter {
         payload = parsePayload(payload)
 
         let broadcast = false
-
         switch (topic) {
           case 'agent/connected':
           case 'agent/disconnected':
@@ -72,7 +111,8 @@ class IotPlatformAgent extends EventEmitter {
     if (this._started) {
       clearInterval(this._timer)
       this._started = false
-      this.emit('disconnected')
+      this.emit('disconnected', this._agentId)
+      this._client.end()
     }
   }
 }
