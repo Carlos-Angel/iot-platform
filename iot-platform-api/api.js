8227795bf721e8eb90948cd06c07ba0bbb9b6242
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-12 00:16:03 -0500

    feat(iot-platform-api): implementación de rutas de la API con los servicios Agent y Metric

--- iot-platform-api/api.js
+++ iot-platform-api/api.js
@@ -13,7 +13,7 @@ api.use('*', async (req, res, next) => {
   if (!services) {
     debug('connected database')
     try {
-      services = await db(config)
+      services = await db(config.db)
     } catch (error) {
       next(error)
     }
@@ -23,28 +23,66 @@ api.use('*', async (req, res, next) => {
   next()
 })
 
-api.get('/agents', (req, res) => {
+api.get('/agents', async (req, res, next) => {
   debug('A request has come to /agents')
-  res.send({})
+  let agents = []
+  try {
+    agents = await Agent.findConnected()
+  } catch (err) {
+    return next(err)
+  }
+  res.send(agents)
 })
 
-api.get('/agent/:uid', (req, res, next) => {
+api.get('/agent/:uid', async (req, res, next) => {
   const { uid } = req.params
+  debug(`request to /agent/${uid}`)
+  let agent
 
-  if (uid !== 'yyy') {
-    return next(new Error('Agent no found'))
+  try {
+    agent = await Agent.findByUuid(uid)
+  } catch (err) {
+    return next(err)
   }
 
-  res.send({ uid })
+  if (!agent) {
+    return next(new Error(`Agent not found with uuid ${uid}`))
+  }
+
+  res.send(agent)
 })
 
-api.get('/metrics/:uid', (req, res) => {
+api.get('/metrics/:uid', async (req, res, next) => {
   const { uid } = req.params
-  res.send({ uid })
+  debug(`request to /metrics/${uid}`)
+  let metrics = []
+
+  try {
+    metrics = await Metric.findByAgentUuid(uid)
+  } catch (err) {
+    return next(err)
+  }
+
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics not found for agent with uuid ${uid}`))
+  }
+  res.send(metrics)
 })
 
-api.get('/metrics/:uid/:type', (req, res) => {
+api.get('/metrics/:uid/:type', async (req, res, next) => {
   const { uid, type } = req.params
-  res.send({ uid, type })
+  debug(`request to /metrics/${uid}/${type}`)
+  let metrics = []
+
+  try {
+    metrics = await Metric.findByTypeAgentUuid(type, uid)
+  } catch (err) {
+    return next(err)
+  }
+
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics (${type}) not found for agent with uuid ${uid}`))
+  }
+  res.send(metrics)
 })
 module.exports = api
--- iot-platform-api/config.js
+++ iot-platform-api/config.js
@@ -2,12 +2,13 @@
 
 const debug = require('debug')('iot-platform:db')
 
-module.export = {
-  database: process.env.DB_NAME || 'iot_platform',
-  username: process.env.DB_USER || 'iot_platform',
-  password: process.env.DB_PASSWORD || 'iot_platform',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-  logging: s => debug(s),
-  setup: true
+module.exports = {
+  db: {
+    database: process.env.DB_NAME || 'iot_platform',
+    username: process.env.DB_USER || 'iot_platform',
+    password: process.env.DB_PASSWORD || 'iot_platform',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s)
+  }
 }
