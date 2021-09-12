d78817fb3b771082426391dd5c01e74b2318cd16
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-11 22:05:08 -0500

    feat(iot-platform-api): definición de rutas de la API

---
+++ iot-platform-api/api.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const debug = require('debug')('iot-platform:api:routes')
+const express = require('express')
+
+const api = express.Router()
+
+api.get('/agents', (req, res) => {
+  debug('A request has come to /agents')
+  res.send({})
+})
+
+api.get('/agent/:uid', (req, res) => {
+  const { uid } = req.params
+  res.send({ uid })
+})
+
+api.get('/metrics/:uid', (req, res) => {
+  const { uid } = req.params
+  res.send({ uid })
+})
+
+api.get('/metrics/:uid/:type', (req, res) => {
+  const { uid, type } = req.params
+  res.send({ uid, type })
+})
+module.exports = api
